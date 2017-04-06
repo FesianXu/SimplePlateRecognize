3afe9f12b542e7e94618e1c66eb2490f90879128
FesianXu <musicxfx@163.com>
2017-04-06 23:58:29 +0800

    begin to add projection transform to modify the plate

--- prac.m
+++ prac.m
@@ -1,54 +1,72 @@
 %%%
 % Author:FesianXu
 % the teaching demo of plate detection
+% version1.0: simple function, not affine transform, not machine learning
+% algorithms,not smart char divide algorithms
 %%%
 clc
 clear
 close all
 
 %% parameter initiation
+%%% 一些参数的初始化和设定
 char_nor_width = 16 ;
 char_nor_height = 32 ;
 plate_nor_width = 160 ;
 plate_nor_height = 48 ;
+plate_wh_upper = 4 ; % 车牌长宽比例上界
+plate_wh_lower = 1.5 ; % 车牌长宽比下界
+char_wh_upper = 2.5 ; % 字符长宽比上界
+char_wh_lower = 1.2 ; % 字符长宽比下界
+
 
 %% get image and turn it to hsv space
-path = 'F:\\opencvjpg\\' ;
-file_name = '17.jpg' ;
+%%% 进行一些预处理之后，根据先验知识初步提取出车牌假设区域。
+path = 'F:\opencvjpg\' ;
+file_name = '41.jpg' ;
 file_path = [path, file_name] ;
 img = imread(file_path) ;
 merge = getBluePlate(img) ;
 % get hsv blue
 core = ones(6,6) ;
 merge = imdilate(merge, core) ;
-% subplot(2,2,1)
-% imshow(img)
-% subplot(2,2,2)
-% imshow(merge)
+subplot(2,2,1)
+imshow(img)
+subplot(2,2,2)
+imshow(merge)
 con = bwboundaries(merge,8, 'noholes') ;
 con_size = size(con) ;
 con_size = con_size(1,1) ;
 save_con = {} ;
 inner = 1 ;
 
-%% search for plate location may be need to judge if it is really plate
+%% search for plate location may be need to judge if it is really plate,delete some area
+%%% 删除掉一些明显不是车牌假设区域的，此时车牌可能会存在仿射变换或者透视变换，需要进一步
+%%% 进行甄别
 for i = 1:con_size
     dy = max(con{i}(:,1))-min(con{i}(:,1)) ;
     dx = max(con{i}(:,2)-min(con{i}(:,2))) ;
-    if dx/dy < 4 && dx/dy > 1.5 && dx > 20 && dy > 20
+    if dx/dy < plate_wh_upper && dx/dy > plate_wh_lower && dx > 20 && dy > 20
         save_con{inner} = con{i} ;
         inner = inner+1 ;
     end
 end
 
+%% here we need to project transform to refine the plates
+%%% 提取出车牌假设区域的四个特征点，映射到新区域中，做透视变换，矫正其倾斜。
+
+
+
 %% get plate and draw plate in source image
+%%% 在原图中绘制出车牌区域。
 [rimg num] = drawPlate(img, save_con) ;
-% subplot(2,2,3)
-% imshow(rimg) % draw plate in origin image
+subplot(2,2,3)
+imshow(rimg) % draw plate in origin image
 [eximg, num] = extractPlate(img, save_con) ;
 eximg = imgNormal(eximg, plate_nor_width, plate_nor_height) ;
 
 %% get one plate to test algorithm
+%%% 取出车牌进行预处理，滤波等
 plate_temp = eximg{1} ; % only process first plate as example
 gray_img = rgb2gray(plate_temp) ;
 hcore = fspecial('gaussian') ;
@@ -59,7 +77,9 @@ core = ones(2,2) ;
 erode_bin = imerode(bin, core) ;
 figure(5)
 imshow(erode_bin)
+
 %% get chars in plate
+%%% 车牌字符分割
 con = bwboundaries(erode_bin,8, 'noholes') ;
 con_size = size(con) ;
 con_size = con_size(1,1) ;
@@ -68,43 +88,53 @@ inner = 1 ;
 for i = 1:con_size
     dy = max(con{i}(:,1))-min(con{i}(:,1)) ;
     dx = max(con{i}(:,2)-min(con{i}(:,2))) ;
-    if dy/dx <= 2.5 && dy/dx >= 1.2 && dx > 5 && dy > 10
+    if dy/dx <= char_wh_upper && dy/dx >= char_wh_lower && dx > 5 && dy > 10
         save_char{inner} = con{i} ;
         inner = inner+1 ;
     end
 end % get char into a cell
 
 %% re-get chars
+%%% 根据先验知识分割车牌字符
 exchar = regetChar(erode_bin, save_char) ;
 exchar = imgNormal(exchar, char_nor_width,char_nor_height) ; % normalize the size of char image
 size_char = size(exchar) ;
 size_char = size_char(1,1) ;
 charset = cell(size_char, 1) ;
+
 %% thining the chars
-for i = 1:size_char
+%%% 细化车牌字符，使得其不会收到一些噪声干扰，前提是有一个较为稳健的特征提取方法。
+% for i = 1:size_char
 %     figure(3)
-    exchar{i} = uint8(exchar{i}*255) ;
-    charset{i} = bwmorph(exchar{i}, 'thin',Inf); % char thining
+%     exchar{i} = uint8(exchar{i}*255) ;
+%     charset{i} = bwmorph(exchar{i}, 'thin',Inf); % char thining
 %     subplot(1, size_char, i)
 %     imshow(exchar{i})
-end 
+% end 
 % 细化笔画
 %%%%%%%%%%%%%%%%%%%up to now, get char, then need to recognize it%%%%%%%%%%
+
+
 %% extract the char features and then classify them
 %%% i could use projection to encode features, but it is not
 %%% scale-invariant
+%%% 提取字符特征，训练分类器识别或者采用模版匹配。
 
-test_path = 'C:\Users\Administrator\Desktop\sample\cha&num\' ;
-test_proj = zeros(35, char_nor_height+char_nor_width) ;
-for i = 1:34
-    name = [test_path, num2str(i),'.bmp'] ;
-    test_img = imread(name) ;
-    test_img_thin = bwmorph(test_img, 'thin',Inf); % char thining
-    proj1 = vertical_projection(test_img) ;
-    proj2 = horizonal_projection(test_img) ;
-    test_proj(i, :) = [proj1; proj2]' ;
-end
+% test_path = 'G:\数据分析集合\plate\cha&num\' ;
+% test_proj = zeros(35, char_nor_height+char_nor_width) ;
+% for i = 1:34
+%     name = [test_path, num2str(i),'.bmp'] ;
+%     test_img = imread(name) ;
+%     test_img_thin = bwmorph(test_img, 'thin',Inf); % char thining
+%     proj1 = vertical_projection(test_img) ;
+%     proj2 = horizonal_projection(test_img) ;
+%     test_proj(i, :) = [proj1; proj2]' ;
+% end
+% save test_proj test_proj
+
+load test_proj.mat
 relation = [] ;
+charpre_list = {} ;
 for i = 2:7
     proj1 = vertical_projection(exchar{i}) ;
     proj2 = horizonal_projection(exchar{i}) ;
@@ -114,7 +144,16 @@ for i = 2:7
         relation(j) = tmp(1,2) ;
     end
     [~, index] = max(relation) ;
-    getCharName(index) 
+    charpre_list{i-1} = getCharName(index) ;
+end
+
+for i =1:size_char
+    figure(3)
+    subplot(1, size_char, i)
+    imshow(exchar{i})
+    if i >= 2
+        title(charpre_list{i-1})
+    end
 end
 
 
