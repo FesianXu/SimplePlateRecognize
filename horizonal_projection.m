f9c40f9cf49bb8eafe4720cea2433c1c65719d63
FesianXu <musicxfx@163.com>
2017-05-05 20:42:22 +0800

    a better project structure

--- horizonal_projection.m
+++
@@ -1,6 +0,0 @@
-function [proj] = horizonal_projection(img)
-[row col] = size(img) ;
-proj = zeros(row,1);
-for i = 1:row
-    proj(i) = sum(img(i, :)) ;
-end
--- vertical_projection.m
+++
@@ -1,6 +0,0 @@
-function [proj] = vertical_projection(img)
-[row col] = size(img) ;
-proj = zeros(col,1);
-for i = 1:col
-    proj(i) = sum(img(:, i)) ;
-end
